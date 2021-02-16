4593f875670f3101916426b06ad3fd34be2976c9
Kaio Christaldo <kkaiochristaldo@gmail.com>
2021-02-16 01:38:21 -0300

    animação do botão de fechar formulario

--- server/public/scripts/login.js
+++ server/public/scripts/login.js
@@ -1,14 +1,43 @@
 
 
     const CreateRoom = () => {
+    
         document.querySelector("#create-room").className = "visible"
+        let close_create = document.querySelector("#create-room>h1");
+        
+        close_create.addEventListener("mouseover", () => {
+            close_create.classList.add("active");
+        
+            setTimeout(() => {
+                close_create.classList.remove("active");
+            }, 2000)
+        })
+
     }
 
     const SearchRoom = () => {
+
         document.querySelector("#search-room").className = "visible"
+
+        let close_create = document.querySelector("#search-room>h1");
+        
+        close_create.addEventListener("mouseover", () => {
+            close_create.classList.add("active");
+        
+            setTimeout(() => {
+                close_create.classList.remove("active");
+            }, 2000)
+        })
     }
 
     const CloseForms = () => {
         document.querySelector("#create-room").className = "invisible"
         document.querySelector("#search-room").className = "invisible"
+    }
+
+
+    function  animateClose (i) {
+    
+        
+
     }
